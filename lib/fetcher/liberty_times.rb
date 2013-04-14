3a926ab8b12510ca84ef5478e236dc56cbb64974
lulalala <mark@goodlife.tw>
2013-04-14 14:46:25 +0800

    Add 自由時報 fetcher

--- lib/fetcher/liberty_times.rb
+++ lib/fetcher/liberty_times.rb
@@ -1,6 +1,7 @@
 class Fetcher::LibertyTimes < Fetcher
   def initialize(url)
-    @url = url
+    @article = Article.new
+    @article.url = url
     @raw = open(url).read.encode('utf-8', 'big5', :invalid => :replace, :undef => :replace, :replace => '')
     @doc = Nokogiri::HTML(@raw)
   end
@@ -11,17 +12,26 @@ class Fetcher::LibertyTimes < Fetcher
 
   #url = 'http://www.libertytimes.com.tw/2013/new/apr/13/today-sp2.htm'
   def fetch
-    a = Article.new(url:@url)
-    a.title = @doc.at_css('#newtitle').text
-    a.company_name = '自由時報'
-    a.content = @doc.at_css('#newsContent span:not(#newtitle)').text
+    @article.title = @doc.at_css('#newtitle').text
+    @article.company_name = '自由時報'
+    @article.content = @doc.css('#newsContent>span:not(#newtitle)>p:not(.picture)').text
 
-    #a.web_published_at = Time.parse(@doc.at_css('#story_update').text)
+    #@article.web_published_at = Time.parse(@doc.at_css('#story_update').text)
 
-    origin = a.content.match(%r{〔(.*)[/／╱](.*)〕})
-    a.reporter_name = origin[1][%r{記者(.+)},1]
-    a.published_at = Time.parse(@doc.at_css('#date').text)
+    @article.reporter_name = parse_reporter_name()
+    @article.published_at = Time.parse(@doc.at_css('#date').text)
 
-    a
+    @article
+  end
+
+  def parse_reporter_name
+    if match = @article.content.match(%r{〔(.*?)[/／╱](.*?)〕})
+      reporter_name = match[1][%r{記者(.+)},1]
+    elsif match = @article.content.match(%r{記者(.+?)[/／╱]})
+      reporter_name = match[1]
+    elsif match = @article.content.match(%r{（文／(.*?)）})
+      reporter_name = match[1]
+    end
+    reporter_name
   end
 end
