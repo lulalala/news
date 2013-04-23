682444ba2027c748732198cb9a49223a7a8ec6e8
lulalala <mark@goodlife.tw>
2013-04-23 23:07:25 +0800

    Allow Liberty Times fetcher to parse both utf-8 and big5 version of the web pages

--- lib/fetcher/liberty_times.rb
+++ lib/fetcher/liberty_times.rb
@@ -3,16 +3,44 @@ class Fetcher::LibertyTimes < Fetcher
     url.include?('libertytimes.com.tw')
   end
 
+  def initialize(url)
+    @article = Article.new
+    @article.url = url
+    @raw = open(url).read
+    @doc = Nokogiri::HTML(@raw)
+    @encoding = :utf8
+    if @doc.meta_encoding == 'big5'
+      @raw = open(url).read.encode('utf-8', 'big5', :invalid => :replace, :undef => :replace, :replace => '')
+      @doc = Nokogiri::HTML(@raw)
+    @encoding = :big5
+    end
+  end
+
   #url = 'http://www.libertytimes.com.tw/2013/new/apr/13/today-sp2.htm'
   def fetch
-    @article.title = @doc.at_css('#newtitle').text
-    @article.company_name = '自由時報'
-    @article.content = @doc.css('#newsContent>span:not(#newtitle)>p:not(.picture)').text
+    if @encoding == :big5
+      @article.title = @doc.at_css('#newtitle').text
+      @article.company_name = '自由時報'
+      @article.content = @doc.css('#newsContent>span:not(#newtitle)>p:not(.picture)').text
 
-    #@article.web_published_at = Time.parse(@doc.at_css('#story_update').text)
+      @article.reporter_name = parse_reporter_name()
+      @article.published_at = Time.parse(@doc.at_css('#date').text)
+    elsif @encoding == :utf8
+      # new layout uses utf-8
+      @article.title = @doc.at_css('#newsti').text
+      @article.company_name = '自由時報'
+      @article.content = @doc.css('#newsc.news_content').text
 
-    @article.reporter_name = parse_reporter_name()
-    @article.published_at = Time.parse(@doc.at_css('#date').text)
+      time = @doc.at_css('.conttime').text[%r{\d{4}/\d{1,2}/\d{1,2} \d{2}:\d{2}}]
+      if time.nil?
+        match = @doc.at_css('.conttime').text.match(%r{(\d{2}):(\d{2})})
+        @article.published_at = DateTime.now.change({:hour => match[1].to_i , :min => match[2].to_i , :sec => 0 })
+      else
+        @article.published_at = Time.parse("#{time}:00")
+      end
+
+      @article.reporter_name = parse_reporter_name()
+    end
 
     clean_up
 
