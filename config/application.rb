0fff3ddd5ce864f31f0bdfaee78f8c3e0cc91359
lulalala <mark@goodlife.tw>
2013-06-12 11:04:12 +0800

    Set default and fallback locales

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,8 @@ module NewsSucks
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :'zh-TW'
+    config.i18n.fallbacks = [:en]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
