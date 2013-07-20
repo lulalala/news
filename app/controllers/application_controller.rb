fc86d3dba93e95c29abe3e3a21ee8d80fabaecf5
lulalala <mark@goodlife.tw>
2013-07-20 21:39:17 +0800

    Skip cancan for HighVoltage pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
 
   after_filter :prepare_unobtrusive_flash
 
-  check_authorization :unless => :devise_controller?
+  check_authorization :unless => :do_not_check_authorization?
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to request.referer, :alert => exception.message
+  end
+  def do_not_check_authorization?
+    respond_to?(:devise_controller?) or
+    self.class == HighVoltage::PagesController
   end
 
   before_filter :store_current_location, :unless => :devise_controller?
