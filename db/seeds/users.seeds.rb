7452f84925041029a7ff2e12ea7ca7e2599838b2
lulalala <mark@goodlife.tw>
2013-07-25 21:09:56 +0800

    Add one guest user

--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -2,4 +2,8 @@ users_hash = []
 (1..99).each do |i|
   users_hash << {id:i,email:"#{i}@example.com"}
 end
+
+# Guest user
+users_hash << {id:100,email:"guest@example.com",guest:true}
+
 User.seed_once(:id, users_hash)
