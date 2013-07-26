b8008ec7f5b61356dd952751596866a1bdda60a3
lulalala <mark@goodlife.tw>
2013-07-26 22:49:36 +0800

    Fix migration (Cannot add a NOT NULL column with default value NULL)

--- db/migrate/20130721033139_add_user_id_to_article_reviews.rb
+++ db/migrate/20130721033139_add_user_id_to_article_reviews.rb
@@ -1,5 +1,5 @@
 class AddUserIdToArticleReviews < ActiveRecord::Migration
   def change
-    add_column :article_reviews, :user_id, :integer, null:false
+    add_column :article_reviews, :user_id, :integer, null:false, default:100
   end
 end
