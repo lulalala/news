5016be56ff87d0cc8c2e4969f7f4dff18da312b0
lulalala <mark@goodlife.tw>
2014-04-08 19:37:55 +0800

    列出媒體數量

--- app/cells/article_cell.rb
+++ app/cells/article_cell.rb
@@ -3,4 +3,10 @@ class ArticleCell < Cell::Rails
     @article = article
     render
   end
+
+  def company_list
+    @counts = Article.count(:group => 'company_name')
+    @counts = @counts.sort_by {|_key, value| -value}
+    render
+  end
 end
